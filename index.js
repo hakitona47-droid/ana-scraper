86c981017cdf68a298ba94919419074b7fa0f453
hakitona47-droid <h.akitona47@gmail.com>
2026-09-04 23:44:11 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+import http from "http";
+
+async function scrape() {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://www.ana.co.jp/ja/jp/international/", {
+    waitUntil: "networkidle2"
+  });
+
+  const title = await page.title();
+  await browser.close();
+  return title;
+}
+
+http.createServer(async (req, res) => {
+  const result = await scrape();
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(result);
+}).listen(8078);
